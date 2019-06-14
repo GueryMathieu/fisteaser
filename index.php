8d26717614ce42e6d69e3f68b076e8b239157f1a
juliengadion <legadijoncte@hotmail.fr>
2019-06-14 13:51:42 +0200

    2nd commit

--- index.php
+++ index.php
@@ -12,6 +12,10 @@
 
     <main>
 
+        <div class="logo">
+            <img src="assets/img/logo3.jpg" alt="">
+        </div>
+
         <div class="container">
 
             <h1>Opening coming soon :</h1>
