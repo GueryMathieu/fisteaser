a49b6d421cc8535d15763d7bbcf7a0de6ee53395
GueryMathieu <hamta15300@gmail.com>
2019-06-14 16:13:49 +0200

    5th commit

--- index.php
+++ index.php
@@ -13,7 +13,27 @@
 
     <main>
 
-     
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+            
+                <h1>Opening coming soon :</h1>
+                
+                <div id="countdown"></div>
+
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-sm">Small modal</button>
+
+                <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-sm">
+                        <div class="modal-content">
+                            ...
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 
     </main>
 
