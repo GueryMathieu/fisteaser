0e7657525a48deae80014f27b3a3b234b597d990
juliengadion <legadijoncte@hotmail.fr>
2019-06-14 15:18:10 +0200

    4th commit

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <title>fisteaser</title>
 </head>
@@ -12,15 +13,7 @@
 
     <main>
 
-        <div class="container">
-
-            <h1>Opening coming soon :</h1>
-
-            <div id="countdown"></div>
-        
-            <input type="button" value="Join Newsletter">
-
-        </div>
+     
 
     </main>
 
