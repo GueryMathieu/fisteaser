e3fa2c3a4d3b7e7165c6cd1958fb4fd414fd8688
GueryMathieu <hamta15300@gmail.com>
2019-06-14 11:29:02 +0200

    first commit

---
+++ assets/js/countdown.js
@@ -0,0 +1,13 @@
+var countDownDate = new Date("Sep 1, 2019 00:01:00").getTime();
+var x = setInterval(function(){
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    }
+);
